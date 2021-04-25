31377869919be5616371f8883de7fd806a51f92e
Ruviel21 <calvinhobbeson.java@gmail.com>
2021-04-25 15:27:43 +0300

    3. Перегрузить метод max для трех чисел [#497555]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,7 +1,17 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left >= right ? left : right;
+    public static int max(int first, int second) {
+        return first >= second ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        int temp = max(first, second);
+        return max(temp, third);
+    }
+
+    public static int max(int first, int second, int third, int forth) {
+        int temp = max(first, second, third);
+        return max(temp, forth);
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -6,28 +6,49 @@ import org.junit.Test;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        int result = Max.max(first, second);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax3To1Then3() {
-        int left = 3;
-        int right = 1;
-        int result = Max.max(left, right);
+        int first = 3;
+        int second = 1;
+        int result = Max.max(first, second);
         int expected = 3;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax3To3ThenAny() {
-        int left = 3;
-        int right = 3;
-        int result = Max.max(left, right);
+        int first = 3;
+        int second = 3;
+        int result = Max.max(first, second);
         int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMaxOutOfThree() {
+        int first = 9;
+        int second = 5;
+        int third = 2;
+        int result = Max.max(first, second, third);
+        int expected = 9;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMaxOutOfFour() {
+        int first = 9;
+        int second = 5;
+        int third = 2;
+        int forth = 16;
+        int result = Max.max(first, second, third, forth);
+        int expected = 16;
         Assert.assertEquals(result, expected);
     }
 }
