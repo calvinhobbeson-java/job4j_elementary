73ec85b012c9cf2ccb5a574cebff13c053533bfd
Ruviel21 <calvinhobbeson.java@gmail.com>
2021-04-25 16:25:42 +0300

    Исправление тестов

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,16 +1,35 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
 
-    public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        result = Point.distance(1, 4, 2, 5);
-        System.out.println("result (1, 4) to (2, 5) " + result);
-        result = Point.distance(2, 2, 3, 6);
-        System.out.println("result (2, 2) to (3, 6) " + result);
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -3,48 +3,22 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class PointTest {
-    @Test
-    public void when00to20then2() {
-        int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
+    public class PointTest {
+        @Test
+        public void when00to20then2() {
+            int expected = 2;
+            Point a = new Point(0, 0);
+            Point b = new Point(0, 2);
+            double out = a.distance(b);
+            Assert.assertEquals(expected, out, 0.01);
+        }
 
-    @Test
-    public void when00to30then3() {
-        int expected = 3;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 3;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when00to40then4() {
-        int expected = 4;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 4;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when00to50then5() {
-        int expected = 5;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 5;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
+        @Test
+        public void when00to30then3() {
+            int expected = 3;
+            Point a = new Point(0, 0);
+            Point b = new Point(3, 0);
+            double out = a.distance(b);
+            Assert.assertEquals(expected, out, 0.01);
+        }
 }
